ead30af906789d219cc85fe11f48c2e12c3f55d1
Cao Nguyen <nguyen@open-vn.org>
2013-12-17 08:21:45 +0700

    edit layout and qqplot function

--- statics/js/index.js
+++ statics/js/index.js
@@ -39,6 +39,16 @@ $(function() {
 		$('#dataset th').show();
 		$('#dataset td').show();
 	});
+
+	$('#btHideTable').click(function(){
+		if($(this).val() == 'Hide Table'){
+			$(this).val('Show Table');
+		}else{
+			$(this).val('Hide Table');
+
+		}
+		$('#dataset').toggle();
+	});
 })
 
 function BoxPlot(obj) {
@@ -78,142 +88,156 @@ function BoxPlot(obj) {
 }
 
 function Graph(data) {
-	Histogram($('#hostogram'), data);
-	QuantilePlot($('#quantileplot'), data);
-	QQPlot($('#qqplot'), data);
-}
-
-function normal_sample() {
-	var x = 0, y = 0, rds, c;
-	do {
-		x = Math.random() * 2 - 1;
-		y = Math.random() * 2 - 1;
-		rds = x * x + y * y;
-	} while (rds == 0 || rds > 1);
-	c = Math.sqrt(-2 * Math.log(rds) / rds); // Box-Muller transform
-	return x * c; // throw away extra sample y * c
-}
-
-function QQPlot(el, data) {
+	$('#graph').empty();
 	$.each(data, function(idx, item){
-		//generate normal sample
-		var samples = [];
-		for(i = 0; i <= item.Data.length; i++){
-			samples.push(item.Mean + Math.sqrt(item.Variance)*normal_sample())
-		}
-		samples.sort();
+		var ul = $('<ul data-tabs="tabs" class="nav nav-tabs"></ul>');
+		ul.append($('<li></li>').append($('<a data-toggle="tab" href="#histogram'+idx+'">Histogram</a>')));
+		ul.append($('<li></li>').append($('<a data-toggle="tab" href="#quantileplot'+idx+'">QuantilePlot</a>')));
+		ul.append($('<li></li>').append($('<a data-toggle="tab" href="#qqplot'+idx+'">QQPlot</a>')));
 
-		var div = $('<div></div>', {height: 400});
-		var scatters = [];
-		for(i = 0; i <= item.Data.length; i++){
-			scatters.push([samples[i], item.Data[i]]);
-		}
+		var div = $('<div></div>', {
+			class: 'tab-content'
+		});
 
-		$(el).append(div);
-		div.highcharts({
-            chart: {
-                type: 'scatter',
-                zoomType: 'xy'
+		var histogram = $('<div></div>', {
+			class: "tab-pane active",
+			id: 'histogram'+idx
+		});
+		div.append(histogram);
+		var quantileplot = $('<div></div>', {
+			class: "tab-pane",
+			id: 'quantileplot'+idx
+		});
+		div.append(quantileplot);
+		var qqplot = $('<div></div>', {
+			class: "tab-pane",
+			id: 'qqplot'+idx
+		});
+		div.append(qqplot);
+
+		$('#graph').append($('<div class="hero-unit"></div>').append('<h1>'+item.Name+'</h1>').append(ul).append(div));
+		Histogram(histogram, item);
+		QuantilePlot(quantileplot, item);
+		QQPlot(qqplot, item);
+	});
+}
+
+function QQPlot(el, item) {
+	//generate normal sample
+	var samples = jstat.seq(-3, 3, item.Data.length);
+	samples.sort();
+
+	var div = $('<div></div>', {height: 400, width: 400});
+	var scatters = [];
+	for(i = 0; i <= item.Data.length; i++){
+		scatters.push([samples[i], item.Data[i]]);
+	}
+
+	$(el).append(div);
+	div.highcharts({
+        chart: {
+            type: 'scatter',
+            zoomType: 'xy'
+        },
+        series: [{
+            name: 'Q-Q Plot',
+            color: 'rgba(223, 83, 83, .5)',
+            data: scatters
+        }]
+    });
+   //  , function(chart) {
+   //  	console.log(chart)
+   //  	chart.renderer.path(['M', 0, 0, 'L', 400, 400])
+   //  	.attr({
+			// 'stroke-width': 2,
+			// stroke: 'red'
+   //      }).add();
+   //  }
+}
+
+function QuantilePlot(el, item) {
+	var div = $('<div></div>', {height: 400, width: 400});
+	var scatters = [];
+	for(i = 0; i <= item.Data.length; i++){
+		scatters.push([i/item.Data.length, item.Data[i]]);
+	}
+
+	$(el).append(div);
+	div.highcharts({
+        chart: {
+            type: 'scatter',
+            zoomType: 'xy'
+        },
+        series: [{
+            name: 'Quantile Plot',
+            color: 'rgba(223, 83, 83, .5)',
+            data: scatters
+        }]
+    });
+}
+
+function Histogram(el, item) {
+	var div = $('<div></div>', {height: 400});
+	$(el).append(div);
+
+	var series = [];
+	for(var i = 0; i < item.Lookup.Value.length; i++){
+		series.push({
+			name: item.Lookup.Value[i].toString(),
+			data: [item.Lookup.Count[i]],
+            tooltip: {
+            	headerFormat: ''
+        	}
+		});
+	}
+
+    div.highcharts({
+        chart: {
+            type: 'column'
+        },
+        xAxis: {
+            categories: '',
+            title: {
+                text: null
+            }
+        },
+        title: {
+        	text: 'Histogram ' + item.Name,
+        },
+        yAxis: {
+            min: 0,
+            title: {
+                text: 'appears (times)',
+                align: 'high'
             },
-            series: [{
-                name: 'test',
-                color: 'rgba(223, 83, 83, .5)',
-                data: scatters
-            }]
-        });
-	});
-}
-
-function QuantilePlot(el, data) {
-	$(el).empty();
-	$.each(data, function(idx, item){
-		var div = $('<div></div>', {height: 400});
-		var scatters = [];
-		for(i = 0; i <= item.Data.length; i++){
-			scatters.push([i/item.Data.length, item.Data[i]]);
-		}
-
-		$(el).append(div);
-		div.highcharts({
-            chart: {
-                type: 'scatter',
-                zoomType: 'xy'
-            },
-            series: [{
-                name: 'test',
-                color: 'rgba(223, 83, 83, .5)',
-                data: scatters
-            }]
-        });
-	});
-}
-
-function Histogram(el, data) {
-	$(el).empty();
-
-	$.each(data, function(idx, item){
-		var div = $('<div></div>', {height: 400});
-		$(el).append(div);
-
-		var series = [];
-		for(var i = 0; i < item.Lookup.Value.length; i++){
-			series.push({
-				name: item.Lookup.Value[i].toString(),
-				data: [item.Lookup.Count[i]],
-	            tooltip: {
-                	headerFormat: ''
-            	}
-			});
-		}
-
-	    div.highcharts({
-	        chart: {
-	            type: 'column'
-	        },
-	        xAxis: {
-	            categories: '',
-	            title: {
-	                text: null
-	            }
-	        },
-	        title: {
-	        	text: 'Histogram ' + item.Name,
-	        },
-	        yAxis: {
-	            min: 0,
-	            title: {
-	                text: 'appears (times)',
-	                align: 'high'
-	            },
-	            labels: {
-	                overflow: 'justify'
-	            }
-	        },
-	        tooltip: {
-	            valueSuffix: ' times'
-	        },
-	        plotOptions: {
-	            bar: {
-	                dataLabels: {
-	                    enabled: true
-	                }
-	            }
-	        },
-	        legend: {
-	            layout: 'vertical',
-	            align: 'right',
-	            verticalAlign: 'top',
-	            x: -40,
-	            y: 100,
-	            floating: true,
-	            borderWidth: 1,
-	            backgroundColor: '#FFFFFF',
-	            shadow: true
-	        },
-	        credits: {
-	            enabled: false
-	        },
-	        series: series
-	    });
-	});
+            labels: {
+                overflow: 'justify'
+            }
+        },
+        tooltip: {
+            valueSuffix: ' times'
+        },
+        plotOptions: {
+            bar: {
+                dataLabels: {
+                    enabled: true
+                }
+            }
+        },
+        legend: {
+            layout: 'vertical',
+            align: 'right',
+            verticalAlign: 'top',
+            x: -40,
+            y: 100,
+            floating: true,
+            borderWidth: 1,
+            backgroundColor: '#FFFFFF',
+            shadow: true
+        },
+        credits: {
+            enabled: false
+        },
+        series: series
+    });
 }
